609ec16aeeb9dd1b4398146beec342f82337916c
AustinK <40644715+AustinKr@users.noreply.github.com>
2026-09-07 14:34:47 -0700

    Fix template loader errors

--- js/template-loader.js
+++ js/template-loader.js
@@ -1,25 +1,30 @@
 const elementNames = ['header', 'nav', 'footer'];
 
 function replacePlaceholder(otherDoc, name) {
-  const template = otherDoc.getElementById(`#${name}-template`);
-  const placeholder = document.getElementById(`#${name}-placeholder`);
+  const template = otherDoc.getElementById(`${name}-template`);
+  const placeholder = document.getElementById(`${name}-placeholder`);
 
-  const imported = document.importNode(template.content, true);
-  placeholder.appendChild(imported);
+  const imported = document.importNode(template, true);
+  placeholder.appendChild(imported.content);
+}
+
+function runWithDoc(path, onGet) {
+  fetch(path)
+    .then(response => {
+      if (!response.ok)
+        throw new Error(`Response status: ${response.status}`);
+      return response.text()
+    })
+    .then(html => {
+      const parser = new DOMParser();
+      const doc = parser.parseFromString(html, 'text/html');
+      onGet(doc);
+    });
 }
 
 for (const id in elementNames) {
   const name = elementNames[id];
-
-  fetch(`../templates/${name}.html`)
-    .then(response => {
-      if (!response.ok)
-        throw new Error(`Response status: ${response.status}`);
-    })
-    .then(response => response.text())
-    .then(html => {
-      const parser = new DOMParser();
-      const otherDoc = parser.parseFromString(html, 'text/html');
-      replacePlaceholder(otherDoc, name);
-    })
+  runWithDoc(`/templates/${name}.html`, otherDoc => {
+    replacePlaceholder(otherDoc, name);
+  });
 }
